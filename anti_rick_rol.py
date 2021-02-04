6705d375cbe1baeab36ce4185a36d80cec56435e
hx00die <74320169+hx00die@users.noreply.github.com>
2021-02-04 19:29:06 +0200

    Add files via upload

---
+++ anti_rick_rol.py
@@ -0,0 +1,28 @@
+import urllib.request
+import json
+import urllib
+import pprint
+
+def is_rick_rol():
+   if "rick" in result or "Rick" in result or "Never" in result:
+       print("Probably a rick roll")
+   else:
+       print("not a rick roll")
+
+input_url = input("enter sus url: ")
+split_url = input_url.split('/') 
+VideoID = split_url[2] 
+
+params = {"format": "json", "url": input_url}
+url = "https://www.youtube.com/oembed"
+query_string = urllib.parse.urlencode(params)
+url = url + "?" + query_string
+
+with urllib.request.urlopen(url) as response:
+    response_text = response.read()
+    data = json.loads(response_text.decode())
+    result = data['title']
+    is_rick_rol()
+    print("Titel: ", result)
+    print("Channel: ", data["author_url"])
+
